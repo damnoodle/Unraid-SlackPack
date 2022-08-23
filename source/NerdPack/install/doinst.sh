a5cd3ca8067500fd4feeb6c8ae40c25f3ab6952c
damnoodle <damnoodle@users.noreply.github.com>
2022-08-23 11:34:45 -0500

    Update doinst.sh

--- source/NerdPack/install/doinst.sh
+++ source/NerdPack/install/doinst.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ln -sf /usr/local/emhttp/plugins/NerdPack/scripts/packagemanager /usr/sbin/packagemanager
+ln -sf /usr/local/emhttp/plugins/SlackPack/scripts/packagemanager /usr/sbin/packagemanager
 chmod +0755 /usr/sbin/packagemanager
